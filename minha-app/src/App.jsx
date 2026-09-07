92448a283cc580634544bb7708b3c274b483d675
Biggiefrane <samuelhenriquefranca77@gmail.com>
2026-09-07 14:57:01 -0300

    TENTATIVAS N 100% FEITAS

---
+++ minha-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "/App.css";
+function App() {
+  return (
+    <div className="App">
+      <h1>Dev Cursos+</h1>
+
+    <p> confia no processo,jaja da bom!</p>
+
+    </div>
+  );
+}
+
+export default App;  
